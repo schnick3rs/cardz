84dfca05a4001de4ba383d45d289cef102145c9f
Moritz Orth <moritz.orth@bold-ventures.de>
2018-05-10 00:48:36 +0200

    add

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var gulp    = require('gulp');
+var sass    = require('gulp-sass');
+var plumber = require('gulp-plumber');
+
+var config = {
+    src: 'app/assets',
+    dest: 'app/resources'
+};
+
+gulp.task('sass', function () {
+  return gulp.src( config.src + '/scss/**/*.scss')
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(gulp.dest( config.dest + '/css/'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch( config.src + '/scss/**', ['sass']);
+});
+
+
+gulp.task('default', [ 'sass' ]);
