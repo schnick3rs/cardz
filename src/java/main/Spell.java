4eb6d53652cccea601e6662eb4b0297662f34a68
Moritz Orth <moritz.orth@bold-ventures.de>
2018-05-10 14:19:07 +0200

    zingz

---
+++ src/java/main/Spell.java
@@ -0,0 +1,26 @@
+
+private String name
+
+private int level;
+
+private boolean isRitual;
+
+private boolean isConcentration;
+
+private int spellComponents;
+
+private String spellMaterialString;
+
+private boolean spellMetarialIsConsumed;
+
+private int spellMaterialCostInGp;
+
+private String castingTime;
+
+private String duration;
+
+private String range;
+
+private String areaOfeffect;
+
+private String description;
